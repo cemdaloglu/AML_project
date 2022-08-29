236db0c08641291684dd27180a93d0417242ef04
Cem Daloglu <chemdaloglu@gmail.com>
2022-08-29 21:43:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from training.train_model import train_model
+from models.unet import UNet
+import torch
+from torchsummary import summary
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('device', device)
+
+
+model = UNet()
+model = model.to(device)
+summary(model, input_size=(4, 64, 64))
+
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001, weight_decay=0.001)
+
+model = train_model(model, optimizer, num_epochs=1)
