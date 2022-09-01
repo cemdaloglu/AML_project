b96ae7bb675a63ebcbca0f45c58b8d57be481334
Cem Daloglu <chemdaloglu@gmail.com>
2022-09-01 02:26:15 +0200

    test dataloader added

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -18,20 +18,40 @@ def get_dataloaders(dataset, train_val_frac, dataloader_workers: int = 3, batch_
 
     kwargs = {'pin_memory': True, 'num_workers': dataloader_workers}
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, 
-        batch_size=batch_size, 
+        train_dataset,
+        batch_size=batch_size,
         shuffle=True,
         drop_last=True,
         **kwargs
     )
     val_loader = torch.utils.data.DataLoader(
-        val_dataset, 
-        batch_size=batch_size, 
+        val_dataset,
+        batch_size=batch_size,
         drop_last=True,
         shuffle=True,
         **kwargs
     )
     return {
-      'train': train_loader,
-      'val': val_loader
+        'train': train_loader,
+        'val': val_loader
     }
+
+
+def get_test_dataloaders(test_dataset, dataloader_workers: int = 3, batch_size: int = 8):
+    """
+    Get Dataloaders for the given dataset.
+
+    @param test_dataset The dataset to wrap into a Dataloader
+    @param dataloader_workers How many workers to give each Dataloader.
+    @param batch_size Batch Size
+    """
+
+    kwargs = {'pin_memory': True, 'num_workers': dataloader_workers}
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=True,
+        **kwargs
+    )
+    return test_loader
