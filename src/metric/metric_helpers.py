42bc32fa4c8e5c4984ebdcfda7c55409e4f11f9a
laierkasten23 <lia.schmid98@gmx.de>
2022-09-12 18:14:05 +0200

    adding the saving of the metrics, need to test whether it works

--- src/metric/metric_helpers.py
+++ src/metric/metric_helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import csv
 
 def print_metrics(metrics, epoch_samples, phase):
     outputs = []
@@ -10,3 +11,17 @@ def print_metrics(metrics, epoch_samples, phase):
 def create_metric_file(path):
     metrics = pd.DataFrame(columns=["Epoch","Phase","Loss", "Accuracy", "F1Score", "Precision", "Recall"])
     metrics.to_csv(path, index=False)
+
+
+def save_metrics(checkpoint_path_metrics, epoch, phase, loss, accuracy, f1Score, precision, recall):
+    all_metrics = pd.read_csv(checkpoint_path_metrics + "metrics.csv")
+    epoch_metrics = {'Epoch': epoch,
+                     'Phase': phase,
+                     'Loss': loss,
+                     'Accuracy': accuracy,
+                     'F1Score': f1Score,
+                     'Precision': precision,
+                     'Recall': recall
+                     }
+    all_metrics = all_metrics.append(epoch_metrics, ignore_index=True)
+    all_metrics.to_csv(checkpoint_path_metrics + "metrics.csv", index=False)
