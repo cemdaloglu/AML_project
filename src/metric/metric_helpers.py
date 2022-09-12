57626879b1be00125b1a5845bd2cde1481f42391
laierkasten23 <lia.schmid98@gmx.de>
2022-09-12 18:56:39 +0200

    to cpu

--- src/metric/metric_helpers.py
+++ src/metric/metric_helpers.py
@@ -17,11 +17,11 @@ def save_metrics(checkpoint_path_metrics, epoch, phase, loss, accuracy, f1Score,
     all_metrics = pd.read_csv(checkpoint_path_metrics + "metrics.csv")
     epoch_metrics = {'Epoch': epoch,
                      'Phase': phase,
-                     'Loss': loss.detach().numpy(),
-                     'Accuracy': accuracy.detach().numpy(),
-                     'F1Score': f1Score.detach().numpy(),
-                     'Precision': precision.detach().numpy(),
-                     'Recall': recall.detach().numpy()
+                     'Loss': loss.cpu().numpy(),
+                     'Accuracy': accuracy.cpu().numpy(),
+                     'F1Score': f1Score.cpu().numpy(),
+                     'Precision': precision.cpu().numpy(),
+                     'Recall': recall.cpu().numpy()
                      }
     all_metrics = all_metrics.append(epoch_metrics, ignore_index=True)
     all_metrics.to_csv(checkpoint_path_metrics + "metrics.csv", index=False)
