79a6a9873ef9ea160dea8f4e71e61bed7c4b3e75
Cem Daloglu <chemdaloglu@gmail.com>
2022-09-14 20:59:27 +0200

    test model update

--- src/training/test_model.py
+++ src/training/test_model.py
@@ -1,5 +1,6 @@
 """ Module for model evaluation """
 
+import time
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -10,6 +11,8 @@ from ..metric.loss import calc_loss
 # helper function to get the root path
 from pathlib import Path
 from ..helpers.visualize import plot_test
+from torchmetrics import Accuracy, F1Score, Precision, Recall, MetricCollection
+from ..metric.metric_helpers import save_metrics
 
 
 def get_project_root() -> Path:
@@ -17,7 +20,7 @@ def get_project_root() -> Path:
     return Path(__file__).parent
 
 
-def test(model, test_loader, use_cuda: bool, loss_criterion=None, n_classes = 5):
+def test(model, test_loader, use_cuda: bool, loss_criterion=None, n_classes = 5, checkpoint_path_metrics = None):
     """
     Compute test metrics on test data set 
 
@@ -28,22 +31,28 @@ def test(model, test_loader, use_cuda: bool, loss_criterion=None, n_classes = 5)
     @param: test_batch_size: -- used batch size
     """
     model.eval()
-    labels = np.arange(n_classes)
-    conf_matrix = np.zeros((n_classes, n_classes))
 
-    # initialize all variables 
-    num_correct = 0
-    num_pixels = 0
+    # initialize all variables
     all_images = []
     all_labels = []
-    all_predictions = []
     test_losses = []
+    since = time.time()
+
+    total_acc = {key: [] for key in ['test']}
+    total_loss = {key: [] for key in ['test']}
+    phase = 'test'
 
     with torch.no_grad():
-        acc = 0
-        pre = 0
-        recall = 0
-        f1 = 0
+        metrics = MetricCollection([
+            Accuracy(ignore_index=0, mdmc_average="global"),
+            F1Score(ignore_index=0, mdmc_average="global"),
+            Precision(ignore_index=0, mdmc_average="global"),
+            Recall(ignore_index=0, mdmc_average="global")
+        ])
+
+        test_metrics = metrics.clone(prefix="test")
+        running_loss = 0
+
         for batch_index, dic in tqdm(enumerate(test_loader), total=len(test_loader)):
             images, labels = dic['image'], dic['mask']
 
@@ -59,33 +68,33 @@ def test(model, test_loader, use_cuda: bool, loss_criterion=None, n_classes = 5)
 
             # compute and save loss
             test_loss = calc_loss(labels.long(), prediction, criterion=loss_criterion)
-            test_losses.extend(test_loss.cpu().numpy().reshape(-1))
+            #test_losses.extend(test_loss.cpu().numpy().reshape(-1))
 
-            # take argmax to get class 
-            final_prediction = torch.argmax(prediction.softmax(dim=1), dim=1)  # torch.Size([batch_size, h, w])
+            # statistics
+            running_loss += test_loss * prediction.size(0)
+            preds_cpu = prediction.argmax(dim=1).cpu()
+            labels_cpu = labels.cpu()
+            test_metrics.update(preds_cpu, labels_cpu)
 
-            for j in range(len(labels)):
-                true_label = labels[j].cpu().detach().numpy().flatten()
-                pred_label = final_prediction[j].cpu().detach().numpy().flatten()
-                conf_matrix += confusion_matrix(true_label, pred_label,
-                                                labels=labels)  # TODO: maybe use to compute IoU (Intersection over Union)
+        computed_metrics = test_metrics.compute()
+        test_metrics.reset()
 
-            all_predictions.extend(final_prediction.cpu())
+        epoch_loss = running_loss / len(test_loader)
+        computed_metrics[f"{phase}Loss"] = epoch_loss
 
-            # Compute number of correct predictions 
-            num_correct += (final_prediction == labels).sum()
-            num_pixels += torch.numel(final_prediction)
+        total_acc[phase].append(computed_metrics[f"{phase}Accuracy"].item())
+        total_loss[phase].append(computed_metrics[f"{phase}Loss"].item())
 
-            #   calculate accuracy
-            acc += accuracy_score(true_label, pred_label) / len(test_loader)
-            #   calculate precision
-            pre += precision_score(true_label, pred_label, average='macro', zero_division=1) / len(test_loader)
-            #   calculate recall
-            recall += recall_score(true_label, pred_label, average='macro', zero_division=1) / len(test_loader)
-            #   calculate F1 score
-            f1 += f1_score(true_label, pred_label, average='macro') / len(test_loader)
-    print('Loss of best validation batch:', np.min(test_losses))
+        # Display metrics in Tensorboard
 
-    losses_per_instance = pd.DataFrame(data={'loss': test_losses})
+        if checkpoint_path_metrics is not None:
+            metrics_list = []
+            for item in ["Loss", "Accuracy", "F1Score", "Precision", "Recall"]:
+                metr = computed_metrics[f"{phase}{item}"]
+                metrics_list.append(metr)
 
-    plot_test(test_losses, acc, pre, recall, f1, conf_matrix, num_correct, num_pixels)
+            save_metrics(checkpoint_path_metrics, phase, *metrics_list)
+
+    # Display total time
+    time_elapsed = time.time() - since
+    print('Total time elapsed: {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
