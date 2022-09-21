6edef2a91841f20f23b1c7835d660cd62bb990e1
laierkasten23 <lia.schmid98@gmx.de>
2022-09-21 14:06:33 +0200

    string problems

--- userfunctions/compare.py
+++ userfunctions/compare.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     print("Reading and Patching data")
     parser = argparse.ArgumentParser()
     parser.add_argument('-best', '--best_model', help='The model which had the highest accuracy to show the entire city prediction', default = "unet_CEL_lr001_hflip_vflip03_wd01", type=str)
-    parser.add_argument('-m_list', '--model_list', help='List of length 4 with the U-Net model, the VGG16 model, VGG16 pretrained model and VGG16 index model', nargs="*" , default = ["unet_CEL_lr001_hflip_vflip03_wd01", "unet_CEL_lr001_hflip_vflip03_wd01", "unet_CEL_lr001_hflip_vflip03_wd01", "unet_CEL_lr001_hflip_vflip03_wd01"], type=list)
+    parser.add_argument('-m_list', '--model_list', help='List of length 4 with the U-Net model, the VGG16 model, VGG16 pretrained model and VGG16 index model', nargs="*" , type=str, default = ["unet_CEL_lr001_hflip_vflip03_wd01", "unet_CEL_lr001_hflip_vflip03_wd01", "unet_CEL_lr001_hflip_vflip03_wd01", "unet_CEL_lr001_hflip_vflip03_wd01"], type=list)
     parser.add_argument('-p_save', '--save_path', help='Path in which to store the merged prediction. "None" if it should not be saved.', default = '../AML_project/img_groundtruth_pred')
 
     args = parser.parse_args()
