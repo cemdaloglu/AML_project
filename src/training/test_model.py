5259b261d302d5b6c3b4c852079f73c830e34e05
laierkasten23 <lia.schmid98@gmx.de>
2022-09-22 21:08:28 +0200

    searching buggy

--- src/training/test_model.py
+++ src/training/test_model.py
@@ -85,7 +85,7 @@ def test(model, test_loader, use_cuda: bool, loss_criterion: str, n_classes: int
                 
                 lab = labels_cpu[ind]
                 # set padded prediction to zero where groundtruth was zero (unlabeled)
-                pred_ignore0 = np.where(lab==0, lab, pred)
+                pred_ignore0 = torch.where(lab==0, lab, pred)
                 print("types = ", type(pred), type(pred_ignore0))
                 num_correct += (pred_ignore0 == lab).sum()
 
